16082fbc6fae9d8e25b767ec280a917e9c174b3e
Fabian Gehring <fabian.gehring@bluewin.ch>
2020-04-19 18:24:09 +0200

    Add plotting abilities for knn

--- R/03-analysis.R
+++ R/03-analysis.R
@@ -72,7 +72,7 @@ calc_payoff_const_gamma <- function(quotes_line, buy = quotes_line$Adjusted_t, s
     I_first <- as.logical(rep(FALSE, n))
     I_first[buy_first & first >= low & first_prev_price > first] <- TRUE
     I_first[sell_first & first <= high & first_prev_price < first] <- TRUE
-
+    
     second_prev_price <- ifelse(I_first, first, quotes_line$Adjusted_t_1)
     I_second <- as.logical(rep(FALSE, n))
     I_second[buy_second & second >= low & second_prev_price > second] <- TRUE
@@ -147,18 +147,20 @@ calc_payoff_const_gamma <- function(quotes_line, buy = quotes_line$Adjusted_t, s
 
 #' Get history of current element
 #'
-#' @param data A sorted data.frame
+#' @param data A sorted data.frame sortest by Ticker and desc(Date)
 #' @param window An integer indicating the length of the history
 #' @param col The column names of the values for which the history should be widened
-#'
+#' @param include_current A logical indicating if the current date (t = 0) should be included in the output
+
 #' @return A data.frame of same length and order like data with the history for each row for the requestes columns. The number at the end of the column tells
-#' their order. 1 is the oldest.
+#' their order and indicates the distance to the current data (i.e. *_7 meaning t-7).
 #' @export
 #'
 #' @examples
-#' data <- tibble(a = 1:10000, b = 1:10000)
-#' test <- widen(quotes_line, 7, "Low")
-widen <- function(data, window, col = names(data)) {
+#' data <- tibble(a = 1:100, b = 1:100)
+#' widen(data, 7, c("a", "b"))
+#' widen(data, 7, c("a", "b"), include_current = TRUE)
+widen <- function(data, window, cols = names(data), include_current = FALSE) {
   
   # fill first rows such that even first row has predecessor
   data_fill <- data[seq_len(window), ] 
@@ -166,19 +168,26 @@ widen <- function(data, window, col = names(data)) {
   data <- bind_rows(data_fill, data)
   
   # widen data
-  row_idx <- unlist(map(seq_len(nrow(data) - window), ~seq_len(window) + . - 1))
-  values_fn <- map(col, ~list) %>% set_names(col)
-  data_widened <- data[row_idx, col] %>%
-    mutate(group_id = rep(seq_len(window), length(row_idx) / window)) %>%
+  row_idx <- map(seq_len(nrow(data) - window), function(x) {
+    curr_row_idx <- seq_len(window) + x - 1
+    if (include_current) {
+      curr_row_idx <- c(curr_row_idx, window + x)
+    }
+    curr_row_idx
+  }) %>% unlist()
+  
+  values_fn <- map(cols, ~list) %>% set_names(cols)
+  data_widened <- data[row_idx, cols] %>%
+    mutate(group_id = rep(rev(seq_len(window + include_current) - include_current), length(row_idx) / (window + include_current))) %>%
     tidyr::pivot_wider(
       names_from = group_id, 
-      values_from = all_of(col), 
+      values_from = all_of(cols), 
       values_fn = values_fn
     ) %>% 
     unnest(cols = names(.)) 
   
   # name columns in case of single feature
-  if (length(col) == 1) names(data_widened) <- paste0(col, "_", names(data_widened))
+  if (length(cols) == 1) names(data_widened) <- paste0(cols, "_", names(data_widened))
   
   data_widened
 }
@@ -187,7 +196,7 @@ widen <- function(data, window, col = names(data)) {
 #' Split data into a query part (the values to search for) and the data part (where to find the data) for a given date.
 #'
 #' @param curr_date The date of the split
-#' @param all_dates The dates for teh rows of data_wide
+#' @param all_dates The dates for the rows of data_wide
 #' @param data_wide A data.frame from where query and data should be extracted
 #'
 #' @return
@@ -196,13 +205,108 @@ widen <- function(data, window, col = names(data)) {
 #'
 #' @examples
 split_data_query <- function(curr_date, all_dates, data_wide) {
-  query <- data_wide[all_dates %in% curr_date, ]
-  data_wide <- data_wide[all_dates < max(curr_date), ]
+  query <- data_wide[all_dates == curr_date, ]
+  data_wide <- data_wide[all_dates < curr_date, ]
   return(list(data_wide = data_wide, query = query))
 }
 
 
-#' Find quote histories in the past that ressembles the current the most
+#' #' Find quote histories in the past that resembles the current the most
+#' #'
+#' #' @param quotes_line A data.frame with quote information
+#' #' @param cols The colum names of the values that should be compared their historical counteroparts
+#' #' @param window The size of the window that should be compared
+#' #' @param k The number of closest neighbors that should be looked for
+#' #' @param chunk_size The size of dates that should be looked at at once. A higher number is faster but if >1 it is not guaranteed that all neigbors found are
+#' #' from the past. Dates with same date as query or later will be returned as NA.
+#' #' @param One of "euclidean" or "manhattan".
+#' #' @param treetype 
+#' #'
+#' #' @return
+#' #' @export
+#' #'
+#' #' @examples
+#' #' data_wide_curr <- tibble(Open_2 = 94, Open_1 = 99, Open_0 = 104, Close_2 = 101, Close_1 = 102, Close_0 = 99)
+#' #' data_wide_nn <- tibble(Open_2 = c(93, 103), Open_1 = c(99, 98), Open_0 = c(103, 98), Close_2 = c(102, 105), Close_1 = c(102, 103), Close_0 = c(98, 99))
+#' find_nn <- function(quotes_line, cols, window, k = 50, chunk_size = 1, norm = "euclidean", mc.cores = parallel::detectCores() - 1) {
+#'   
+#'   data_wide <- quotes_line %>%
+#'     group_by(Ticker) %>% 
+#'     dplyr::group_modify(~widen(data = ., window = window, cols = cols)) %>% 
+#'     ungroup() %>%
+#'     select(-Ticker)
+#'   
+#'   all_dates <- quotes_line$Date
+#'   
+#'   # remove NA values
+#'   na_free_row_bool <- rowSums(is.na(data_wide)) == 0
+#'   data_wide <- data_wide[na_free_row_bool, ]
+#'   all_dates <- all_dates[na_free_row_bool]
+#'   
+#'   
+#'   # order dates such that indizes are strictly increasing
+#'   date_order <- order(all_dates)
+#'   all_dates <- all_dates[date_order]
+#'   data_wide <- data_wide[date_order, ]
+#'   
+#'   unique_dates <- sort(unique(all_dates))
+#'   
+#'   chunk_size <- chunk_size
+#'   n_chunks <- ceiling(length(unique_dates) / chunk_size)
+#'   date_chunks <- split(unique_dates, head(rep(seq_len(n_chunks), each = chunk_size), length(unique_dates)))
+#'   
+#'   #  choose correct nearest neighbor search distance
+#'   if (norm == "euclidean") {
+#'     nn_fct <- RANN::nn2
+#'   } else if (norm == "manhattan") {
+#'     nn_fct <- RANN.L1::nn2
+#'   } else {
+#'     stop("Unexpected value for function argument norm.")
+#'   }
+#'   
+#'   create_dummy_return <- function(width, height) {
+#'     out <- list()
+#'     tmp <- matrix(rep(NA, width * height), ncol = width)
+#'     out$nn.idx <- tmp
+#'     out$nn.dists <- tmp
+#'     out
+#'   }
+#'   
+#'   knn_output <- pbmcapply::pbmclapply(seq_along(date_chunks), function(i) {
+#'     
+#'     curr_date_chunk <- date_chunks[[i]]
+#'     data_query <- split_data_query(curr_date_chunk, all_dates, data_wide)
+#'     
+#'     if (chunk_size == 1 && i == 1) {
+#'       out <- create_dummy_return(k, nrow(data_query$query))
+#'       return(out)
+#'     }
+#'     
+#'     out <- nn_fct(data_query$data_wide, query = data_query$query, min(k, nrow(data_query$data_wide)), treetype = "kd")
+#'     if (nrow(data_query$data_wide) < k) {
+#'       tmp <- create_dummy_return(k, nrow(data_query$query))
+#'       tmp$nn.idx[, seq_len(nrow(data_query$data_wide))] <- out[["nn.idx"]]
+#'       tmp$nn.dists[, seq_len(nrow(data_query$data_wide))] <- out[["nn.dists"]]
+#'       out <- tmp
+#'     }
+#'     return(out)
+#'   }, mc.cores = mc.cores)
+#'   
+#'   nn <- list()
+#'   nn[["nn.idx"]] <- purrr::map(knn_output,  ~.[["nn.idx"]]) %>% do.call(rbind, .)
+#'   nn[["nn.dists"]] <- purrr::map(knn_output,  ~.[["nn.dists"]]) %>% do.call(rbind, .)
+#'   
+#'   # make sure all neighbors are from previous dates
+#'   neighbor_date_minus_self_date <- matrix(all_dates[nn[["nn.idx"]]], ncol = k) - as.integer(all_dates)
+#'   nn[["nn.idx"]][neighbor_date_minus_self_date >= 0] <- NA
+#'   nn[["nn.dists"]][neighbor_date_minus_self_date >= 0] <- NA
+#'   
+#'   nn
+#' }
+#' 
+
+
+#' Find quote histories in the past that resembles the current the most
 #'
 #' @param quotes_line A data.frame with quote information
 #' @param cols The colum names of the values that should be compared their historical counteroparts
@@ -210,85 +314,164 @@ split_data_query <- function(curr_date, all_dates, data_wide) {
 #' @param k The number of closest neighbors that should be looked for
 #' @param chunk_size The size of dates that should be looked at at once. A higher number is faster but if >1 it is not guaranteed that all neigbors found are
 #' from the past. Dates with same date as query or later will be returned as NA.
+#' @param One of "euclidean" or "manhattan".
 #' @param treetype 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-find_nn <- function(quotes_line, cols, window, k = 51, chunk_size = 20, norm = c("euclidean", "manhattan")) {
+#' dates <- as.Date(c("2020-01-03", "2020-01-02", "2020-01-02"))
+#' data_wide <- tibble(
+#'   Open_2 = c(93, 103, 104), 
+#'   Open_1 = c(99, NA, 100), 
+#'   Open_0 = c(103, 98, 100), 
+#'   Close_2 = c(102, 105, 98), 
+#'   Close_1 = c(102, 103, 101), 
+#'   Close_0 = c(98, 99, 103)
+#' )
+#' find_nn(data_wide, dates)
+#' widen(quotes_line, )
+find_nn <- function(data_wide, dates, k = 50, norm = "euclidean", mc.cores = parallel::detectCores() - 1) {
   
-  data_wide <- quotes_line %>% 
-    group_by(Ticker) %>% 
-    dplyr::group_modify(~widen(data = ., window = window, cols = cols)) %>% 
-    ungroup() %>%
-    select(-Ticker)
-  
-  all_dates <- quotes_line$Date
-  
-  # remove NA values
-  na_free_row_bool <- rowSums(is.na(data_wide)) == 0
-  data_wide <- data_wide[na_free_row_bool, ]
-  all_dates <- all_dates[na_free_row_bool]
+  n <- nrow(data_wide)
   
   # order dates such that indizes are strictly increasing
-  date_order <- order(all_dates)
-  all_dates <- all_dates[date_order]
-  data_wide <- data_wide[date_order, ]
+  date_order <- order(dates)
+  dates_ordered <- dates[date_order]
+  data_wide_ordered <- data_wide[date_order, ]
+  # rm(data_wide)
   
-  unique_dates <- sort(unique(all_dates))
-  
-  chunk_size <- chunk_size
-  n_chunks <- ceiling(length(unique_dates) / chunk_size)
-  date_chunks <- split(unique_dates, head(rep(seq_len(n_chunks), each = chunk_size), length(unique_dates)))
-  
+
   #  choose correct nearest neighbor search distance
   if (norm == "euclidean") {
-    nn_fct <- RANN::nn2(data_query$data_wide, query = data_query$query, min(k, nrow(data_query$data_wide)), treetype = "kd")
+    nn_fct <- RANN::nn2
   } else if (norm == "manhattan") {
-    nn_fct <- RANN.L1::nn2(data_query$data_wide, query = data_query$query, min(k, nrow(data_query$data_wide)), treetype = "kd")
+    nn_fct <- RANN.L1::nn2
   } else {
     stop("Unexpected value for function argument norm.")
   }
   
-  knn_output <- pbmcapply::pbmclapply(date_chunks, function(curr_date_chunk) {
-    data_query <- split_data_query(curr_date_chunk, all_dates, data_wide)
+  create_dummy_return <- function(width, height) {
+    out <- list()
+    tmp <- matrix(rep(NA, width * height), ncol = width)
+    out$nn.idx <- tmp
+    out$nn.dists <- tmp
+    out
+  }
+
+  na_row_bool <- rowSums(is.na(data_wide_ordered)) > 0
+  data_wide_filtered <- data_wide_ordered[!na_row_bool, ]
+  dates_filtered <- dates_ordered[!na_row_bool]
+
+  unique_dates <- sort(unique(dates_filtered))
+  knn_output <- pbmcapply::pbmclapply(seq_along(unique_dates), function(i) {
+
+    curr_date <- unique_dates[i]
+    data_query <- split_data_query(curr_date, dates_filtered, data_wide_filtered)
+    # rm(data_wide_ordered, dates)
+
+    if (i == 1) {
+      out <- create_dummy_return(k, nrow(data_query$query))
+      return(out)
+    }
     
     out <- nn_fct(data_query$data_wide, query = data_query$query, min(k, nrow(data_query$data_wide)), treetype = "kd")
     
     if (nrow(data_query$data_wide) < k) {
-      tmp <- matrix(rep(NA, k * nrow(data_query$query)), ncol = k)
-      nn.idx <- tmp
-      nn.idx[, seq_len(nrow(data_query$data_wide))] <- out[["nn.idx"]]
-      nn.dists <- tmp
-      nn.dists[, seq_len(nrow(data_query$data_wide))] <- out[["nn.dists"]]
-      out <- list(nn.idx = nn.idx, nn.dists = nn.dists)
+      tmp <- create_dummy_return(k, nrow(data_query$query))
+      tmp$nn.idx[, seq_len(nrow(data_query$data_wide))] <- out[["nn.idx"]]
+      tmp$nn.dists[, seq_len(nrow(data_query$data_wide))] <- out[["nn.dists"]]
+      out <- tmp
     }
     return(out)
-  }, mc.cores = parallel::detectCores() - 1)
+  }, mc.cores = mc.cores)
   
   nn <- list()
-  nn[["nn.idx"]] <- purrr::map(knn_output,  ~.[["nn.idx"]]) %>% do.call(rbind, .)
-  nn[["nn.dists"]] <- purrr::map(knn_output,  ~.[["nn.dists"]]) %>% do.call(rbind, .)
   
-  # make sure all neighbors are from previous dates
-  neighbor_date_minus_self_date <- matrix(all_dates[nn[["nn.idx"]]], ncol = k) - as.integer(all_dates)
-  nn[["nn.idx"]][neighbor_date_minus_self_date >= 0] <- NA
-  nn[["nn.dists"]][neighbor_date_minus_self_date >= 0] <- NA
+  # make sure also excluded rows are in the result (as NA rows)
+  nn[["nn.idx"]] <- matrix(rep(NA_integer_, k * n), ncol = k)
+  nn[["nn.idx"]][!na_row_bool, ] <- purrr::map(knn_output,  ~.[["nn.idx"]]) %>% do.call(rbind, .)
   
+  # take into account in indices that there were excluded rows
+  nn[["nn.idx"]] <- matrix(seq_len(nrow(data_wide_ordered))[!na_row_bool][nn[["nn.idx"]]], ncol = k)
+  
+  nn[["nn.dists"]] <- matrix(rep(NA_real_, k * n), ncol = k)
+  nn[["nn.dists"]][!na_row_bool, ] <- purrr::map(knn_output,  ~.[["nn.dists"]]) %>% do.call(rbind, .)
+  
+  # order result in the same way as inputed data
+  nn[["nn.idx"]] <- nn[["nn.idx"]][order(date_order), ]
+  nn[["nn.idx"]] <- matrix(date_order[nn[["nn.idx"]]], ncol = k)
+  
+  nn[["nn.dists"]] <- nn[["nn.dists"]][order(date_order), ]
+
   nn
 }
 
 
-# suppressPackageStartupMessages({
-#   library(dplyr)
-#   library(stringr)
-#   library(purrr)
-#   library(pbmcapply)
-#   library(kknn)
-#   library(tidyr)
-# })
-# nn <- find_nn(quotes_line, "Low", 5, chunk_size = 1)
 
-y <- 12324156
 
+#' Plot price movements for window and current time frame inkl. prediction
+#'
+#' @param data_wide_curr A data.frame with the history of quotes for the Ticker and Date of interest. Includes at least the columns given in cols followed by 
+#' a window suffix (i.e "Close_3")
+#' @param data_wide_nn A data.frame with the history of quotes for the nearest neighboars. Includes at least the columns given in cols followed by a window 
+#' suffix (i.e "Close_3")
+#'
+#' @return A ggplot2 object
+#' @export
+#'
+#' @examples
+#' data_wide_curr <- tibble(Open_2 = 94, Open_1 = 99, Open_0 = 104, Close_2 = 101, Close_1 = 102, Close_0 = 99)
+#' data_wide_nn <- tibble(Open_2 = c(93, 103), Open_1 = c(99, 98), Open_0 = c(103, 98), Close_2 = c(102, 105), Close_1 = c(102, 103), Close_0 = c(98, 99))
+#' plot_nn(data_wide_curr, data_wide_nn, cols = c("Open", "Close"))
+#' widen(quotes_line, 10, feature_cols = c("Open", "Low", "High", "Close"), target_col = "High")
+plot_nn <- function(data_wide_curr, data_wide_nn, cols = c("Open", "Low", "High", "Close")) {  
+  
+  plot_data <- mutate(data_wide_nn, src = "nn") %>%
+    bind_rows(mutate(data_wide_curr, src = "curr")) %>%
+    mutate(entry = seq_len(n())) %>%
+    tidyr::pivot_longer(-c("entry", "src"), names_to = "type") %>%
+    separate(type, sep = "_", into = c("type", "window"))
+  
+  # factor to ensure order
+  window_string <- ifelse(plot_data$window == 0, "t", paste0("t-", plot_data$window))
+  window_levels <- ifelse(sort(unique(plot_data$window), decreasing = TRUE) == 0, "t", paste0("t-", sort(unique(plot_data$window), decreasing = TRUE)))
+  plot_data <- plot_data %>%
+    mutate_at("window", ~factor(window_string, window_levels)) %>%
+    mutate_at("type", ~factor(., levels = cols))
+  
+  plot_data_nn <- plot_data %>% 
+    filter(src == "nn") %>%
+    mutate_at("entry", ~factor(., levels = sort(unique(entry))))
+  
+  plot_data_curr <- plot_data %>% filter(src == "curr")
+
+  plot_data_pred <- plot_data_nn %>%
+    filter(window == "t") %>%
+    group_by(type, window) %>%
+    summarise(value = mean(value)) %>%
+    ungroup() %>%
+    mutate(entry = max(plot_data$entry) + 1)
+  
+  ggplot2::ggplot(mapping = ggplot2::aes(x = type, y = value, group = entry)) +
+    
+    # t-x
+    ggplot2::geom_point(data = plot_data_nn) +
+    ggplot2::geom_line(data = plot_data_nn) +
+
+    # t
+    ggplot2::geom_point(data = plot_data_curr, color = "red", size = 1.5) +
+    ggplot2::geom_line(data = plot_data_curr, color = "red", linetype = "solid") +
+
+    # pred t
+    ggplot2::geom_point(data = plot_data_pred, color = "red", alpha = 1) + 
+    ggplot2::geom_line(data = plot_data_pred, color = "red", linetype = "dashed", alpha = 1) + 
+     
+    ggplot2::theme_bw() +
+    ggplot2::facet_grid(rows = ~window) +
+    ggplot2::ggtitle("Dies ist ein Testtitel") + 
+    ggplot2::xlab(NULL) + 
+    ggplot2::ylab(NULL)
+  
+}
