917b2ad74304e8b2f537d8468dcec2b31f780154
Ubuntu <ubuntu@ip-172-31-81-251.ec2.internal>
2020-05-30 22:07:05 +0000

    Parallelize densitiy eval

--- src/find_best_buy_sell.cpp
+++ src/find_best_buy_sell.cpp
@@ -3,6 +3,7 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 IntegerVector find_best_buy_sell(NumericMatrix low_prob, NumericMatrix high_prob, NumericMatrix close_prob, NumericMatrix payoffs) {
+  
   const int n_data = low_prob.nrow();
   const int n_buy_sell = payoffs.ncol();
   const int n_group_low = low_prob.ncol();
@@ -12,11 +13,6 @@ IntegerVector find_best_buy_sell(NumericMatrix low_prob, NumericMatrix high_prob
   IntegerVector out = IntegerVector(n_data);
   
   for (int i = 0; i<n_data; i++) {
-  
-    if (i % 1000 == 0) {
-      Rprintf("%i\n", i);
-    }
-
     double best_payoff = 0.0;
     int best_buy_sell_id = -1;
     for (int buy_sell_id = 0; buy_sell_id < n_buy_sell; buy_sell_id++) {
@@ -41,11 +37,10 @@ IntegerVector find_best_buy_sell(NumericMatrix low_prob, NumericMatrix high_prob
 }
 
 /*** R
-
-low_prob <- matrix(rep(0.1, 30 * 100000), ncol = 30)
-high_prob <- matrix(rep(0.1, 30 * 100000), ncol = 30)
-close_prob <- matrix(rep(0.1, 30 * 100000), ncol = 30)
-payoffs <- matrix(rep(1:10000, 750), ncol = 750)
-
-a <- system.time({find_best_buy_sell(low_prob, high_prob, close_prob, payoffs)})
+# low_prob <- matrix(rep(0.1, 30 * 100), ncol = 30)
+# high_prob <- matrix(rep(0.1, 30 * 100), ncol = 30)
+# close_prob <- matrix(rep(0.1, 30 * 100), ncol = 30)
+# payoffs <- matrix(rep(1:10000, 750), ncol = 750)
+# 
+# find_best_buy_sell(low_prob, high_prob, close_prob, payoffs)
 */
