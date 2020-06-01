bf94e1073f90aab7b503070e6108c974e5c4ca30
Ubuntu <ubuntu@ip-172-31-94-60.ec2.internal>
2020-06-01 07:48:18 +0000

    Validate nn optimizer

--- src/find_best_buy_sell2.cpp
+++ src/find_best_buy_sell2.cpp
@@ -2,7 +2,7 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-IntegerVector find_best_buy_sell(NumericMatrix low_prob, NumericMatrix high_prob, NumericMatrix close_prob, NumericMatrix payoffs) {
+IntegerVector find_best_buy_sell2(NumericMatrix low_prob, NumericMatrix high_prob, NumericMatrix close_prob, NumericMatrix payoffs) {
   
   const int n_data = low_prob.nrow();
   const int n_buy_sell = payoffs.ncol();
@@ -10,21 +10,29 @@ IntegerVector find_best_buy_sell(NumericMatrix low_prob, NumericMatrix high_prob
   const int n_group_high = high_prob.ncol();
   const int n_group_close = close_prob.ncol();
   
+  const int n_sceanrio = n_group_low * n_group_high * n_group_close;
+  
   IntegerVector out = IntegerVector(n_data);
   
   for (int i = 0; i<n_data; i++) {
+    NumericVector prob = NumericVector(n_group_low*n_group_high*n_group_close);
+    
+    int counter = 0;
+    for (int group_low = 0; group_low < n_group_low; group_low++) {
+      for (int group_high = 0; group_high < n_group_high; group_high++) {
+        for (int group_close = 0; group_close < n_group_close; group_close++) {
+          prob[counter] = low_prob(i, group_low) * high_prob(i, group_high) * close_prob(i, group_close);
+          counter++;
+        }
+      }
+    }
+    
     double best_payoff = 0.0;
     int best_buy_sell_id = -1;
     for (int buy_sell_id = 0; buy_sell_id < n_buy_sell; buy_sell_id++) {
       double curr_payoff = 0.0;
-      int price_id = 0;
-      for (int group_low = 0; group_low < n_group_low; group_low++) {
-        for (int group_high = 0; group_high < n_group_high; group_high++) {
-          for (int group_close = 0; group_close < n_group_close; group_close++) {
-            curr_payoff += low_prob(i, group_low) * high_prob(i, group_high) * close_prob(i, group_close) * payoffs(price_id, buy_sell_id);
-            price_id++;
-          }
-        }
+      for (int scenario_id = 0; scenario_id < n_sceanrio; scenario_id++) {
+        curr_payoff += prob[scenario_id] * payoffs(scenario_id, buy_sell_id);
       }
       if (curr_payoff > best_payoff) {
         best_payoff = curr_payoff;
@@ -37,21 +45,12 @@ IntegerVector find_best_buy_sell(NumericMatrix low_prob, NumericMatrix high_prob
 }
 
 /*** R
-# n <- 100
+# set.seed(123)
+# n <- 1000
 # low_prob <- matrix(rnorm(30*n), ncol = 30)
 # high_prob <- matrix(rnorm(30*n), ncol = 30)
 # close_prob <- matrix(rnorm(30*n), ncol = 30)
 # payoffs <- matrix(rnorm(30^3 * 750), ncol = 750)
 # 
 # find_best_buy_sell(low_prob, high_prob, close_prob, payoffs)
-
-### Validation (for element 1)
-# element_id <- 1
-# prob <- expand_grid(
-#   low_prob = as.vector(low_prob[element_id, ]),
-#   high_prob = as.vector(high_prob[element_id, ]),
-#   close_prob = as.vector(close_prob[element_id, ])
-# ) %>% mutate(prob = low_prob * high_prob*close_prob) %$% prob
-# which.max(map_dbl(as_tibble(payoffs) %>% as.list(), ~sum(.*prob)))
-
 */
